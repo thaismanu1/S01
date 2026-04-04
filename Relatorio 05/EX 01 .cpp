4eea284658eb84113350a2a2b91efeb7639298ce
Thaís <112191628+thaismanu1@users.noreply.github.com>
2026-04-03 21:44:52 -0300

    Create EX 01 .cpp

---
+++ Relatorio 05/EX 01 .cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+// função recursiva fibonacci
+int fibonacci(int n) {
+    if (n == 0) {
+        return 0;
+    }
+    else if (n == 1) {
+        return 1;
+    }
+    else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
+
+int main() {
+    int n;
+
+    cout << "Digite um numero: ";
+    cin >> n;
+
+    cout << "Fibonacci de " << n << " = " << fibonacci(n) << endl;
+
+    return 0;
+}
